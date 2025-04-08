350bdbec670e2a258581baf96d05630da0589af1
me-hana <hayonleee@gmail.com>
2025-04-08 16:07:05 +0900

    feat: useMemo.ts 구현

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
-  _deps: DependencyList,
-  _equals = shallowEquals,
+  deps: DependencyList,
+  equals = shallowEquals
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  // Step 1: 이전 deps와 결과를 저장할 ref 만들기
+  const cache = useRef<{ deps: DependencyList; value: T } | null>(null);
+
+  // Step 2: 이전 값이 존재하고, deps가 같으면
+  if (cache.current?.deps && equals(deps, cache.current.deps)) {
+    return cache.current.value;
+  }
+
+  // Step 3: 새로 계산하고, 캐시 갱신
+  const value = factory();
+  cache.current = { deps, value };
+
+  return value;
 }
