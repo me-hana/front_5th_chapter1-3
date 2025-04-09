b00d1ea2f8693e5abc449348cf28e4291e79f754
me-hana <hayonleee@gmail.com>
2025-04-09 16:23:57 +0900

    feat: memo.ts 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { FunctionComponent } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
-  Component: ComponentType<P>,
-  _equals = shallowEquals,
+  Component: FunctionComponent<P>,
+  equals = shallowEquals,
 ) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const previous = useRef<{ props: P; result: React.ReactElement } | null>(
+      null,
+    );
+
+    // 1. 이전 값이 있고, props가 같다면 캐시된 결과 반환
+    if (previous.current && equals(previous.current.props, props)) {
+      return previous.current.result;
+    }
+
+    // 2. 아니면 새로 렌더해서 결과 저장
+    const result = Component(props) as React.ReactElement;
+    previous.current = { props, result };
+    return result;
+  };
 }
